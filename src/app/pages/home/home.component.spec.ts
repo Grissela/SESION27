ff363f0372a57f6fd9c34ed67b9b75f609bc2956
Grissela <108595899+Grissela@users.noreply.github.com>
2023-01-26 15:58:07 -0500

    Solucionar error de test de importacion de pagesModule

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PagesModule } from '../pages.module';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +9,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports:[PagesModule] /*Importar el pagesmodule aqui para que no hubiera error del formsModule del input*/
     })
     .compileComponents();
 
